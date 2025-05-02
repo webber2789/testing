3506e592b3c6e262f9ced8633fde34da5c15de11
webber2789 <cmrnothinker@gmail.com>
2025-05-02 11:04:43 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -2,8 +2,8 @@ local a = false;
 local b = settings();
 
 game:GetService("UserInputService").InputEnded:connect(function(i)
-    if i.KeyCode == Enum.KeyCode.NumLock then
-        a = not a;
-        b.Network.IncomingReplicationLag = a and 1000 or 0;
-    end
+if i.KeyCode == Enum.KeyCode.F3 then
+a = not a;
+b.Network.IncomingReplicationLag = a and 1000 or 0;
+end
 end)
