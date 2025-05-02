8f55c420696e20fca307931cf21151a5cd6f3e2d
webber2789 <cmrnothinker@gmail.com>
2025-05-02 11:18:52 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,10 +1,9 @@
-local a = false
-local b = game:GetService("ReplicatedStorage")
+local a = false;
+local b = settings();
 
 game:GetService("UserInputService").InputEnded:connect(function(i)
-    if i.KeyCode == Enum.KeyCode.NumLock then
-        a = not a
-
+    if i.KeyCode == Enum.KeyCode.NumLock then 
+        a = not a;
+        b.Network.IncomingReplicationLag = a and 1000 or 0;
     end
 end)
-
